408c9b76bed7730e22b769c8382627f552fb4f0d
ccccz <171250013@smail.nju.edu.cn>
2019-09-05 23:53:58 +0800

    [feat]添加原始地图

--- app/src/main/java/com/example/dachuang/MainActivity.java
+++ app/src/main/java/com/example/dachuang/MainActivity.java
@@ -4,13 +4,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private AMap aMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapView=(MapView)findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+
+        init();
+
     }
 
-    //test
+    /**
+     * 初始化aMap对象
+     */
+    private void init(){
+        if(aMap==null){
+            aMap=mapView.getMap();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+
 }
